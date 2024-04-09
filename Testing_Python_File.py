80f4a15b58a76861deccbaaeb5b47aeceb84940d
rjm-16 <rjm251@exeter.ac.uk>
2024-04-09 19:04:05 +0100

    First day of coding - working out how to access table data in website source code and adding to newly created csv file

--- Testing_Python_File.py
+++ Testing_Python_File.py
@@ -8,7 +8,11 @@ Created on Tue Apr  9 13:06:17 2024
 from bs4 import BeautifulSoup
 
 # Sample HTML source code
-html_source = """
+with open('source_html_code.txt', 'r', encoding="utf-8") as file:
+    html_source = file.read()
+
+
+short_html_source = """
 <tr>
     <th class="fiso-lab-table__row-heading">
         <span class="fiso-lab-table__row-heading-rank" title="Rank: 1">1</span>
@@ -38,15 +42,48 @@ html_source = """
 </tr>
 """
 
+
 # Parse the HTML
 soup = BeautifulSoup(html_source, 'html.parser')
 
+# Extract column titles - th code?
+# table_titles = soup.find_all('th')
+
 # Extract data
-table_rows = soup.find_all('tr')
-print(table_rows)
+TAB = soup.find('table', class_="fiso-lab-table")
+table_rows = TAB.find_all('tr')
+#print(table_rows)
 
 for row in table_rows:
+    name_span = row.find('span', class_='fiso-lab-table__row-heading-primary-data')
+    team_span = row.find('span', class_='fiso-lab-table__row-heading-secondary-data')
+        
+    if name_span and team_span:
+        name = name_span.get_text(strip=True)
+        team = team_span.get_text(strip=True)
+        print("Name:", name)
+        print("Team:", team)
+        print()
+    
     data_cells = row.find_all('td')
     for cell in data_cells:
         print(cell.text, end='\t')
     print()  # Newline after each row
+    
+# table heading lines
+th_elements = TAB.find('thead').find_all('th')
+
+# Extract the titles from the title attribute of the <button> elements inside the <th> elements
+column_titles = [th.find('button').get('title') for th in th_elements if th.find('button')]
+
+# Add "Name" and "Club" at the start of the list
+column_titles = ["Name", "Club"] + column_titles
+
+# Print the column titles
+print(column_titles)
+
+with open('Super_Six_Data.csv', 'w') as file:
+    file.write(','.join(column_titles))
+
+
+    
