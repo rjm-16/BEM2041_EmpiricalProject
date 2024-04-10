73eedb5b914cfbe82ef36b3c55db422e4f541340
rjm-16 <rjm251@exeter.ac.uk>
2024-04-10 14:29:21 +0100

    Table scraping complete in Testing_Python_File and first attempts at moving through website in WebScrapingFirstAttempts.py

--- Testing_Python_File.py
+++ Testing_Python_File.py
@@ -5,70 +5,21 @@ Created on Tue Apr  9 13:06:17 2024
 @author: User
 """
 
+from urllib import request, error
 from bs4 import BeautifulSoup
+import re
 
 # Sample HTML source code
 with open('source_html_code.txt', 'r', encoding="utf-8") as file:
     html_source = file.read()
 
 
-short_html_source = """
-<tr>
-    <th class="fiso-lab-table__row-heading">
-        <span class="fiso-lab-table__row-heading-rank" title="Rank: 1">1</span>
-        <span class="fiso-lab-table__row-heading-title">
-            <span class="fiso-lab-table__row-heading-primary-data" title="Le Roux Roets">L. Roets</span>
-            <span class="fiso-lab-table__row-heading-secondary-data" title="Sharks">(SHA)</span>
-        </span>
-    </th>
-    <td class="fiso-lab-table__sorted-column">7</td>
-    <td class="">0</td>
-    <td class="">0</td>
-    <td class="">0</td>
-    <td class="">19</td>
-    <td class="">16</td>
-    <td class="">60</td>
-    <td class="">2</td>
-    <td class="">0</td>
-    <td class="">0</td>
-    <td class="">2</td>
-    <td class="">2</td>
-    <td class="">5</td>
-    <td class="">2</td>
-    <td class="">0</td>
-    <td class="">0</td>
-    <td class="">0</td>
-    <td class="">0</td>
-</tr>
-"""
-
-
 # Parse the HTML
 soup = BeautifulSoup(html_source, 'html.parser')
 
-# Extract column titles - th code?
-# table_titles = soup.find_all('th')
-
-# Extract data
+# Extract table and split into rows of data
 TAB = soup.find('table', class_="fiso-lab-table")
 table_rows = TAB.find_all('tr')
-#print(table_rows)
-
-for row in table_rows:
-    name_span = row.find('span', class_='fiso-lab-table__row-heading-primary-data')
-    team_span = row.find('span', class_='fiso-lab-table__row-heading-secondary-data')
-        
-    if name_span and team_span:
-        name = name_span.get_text(strip=True)
-        team = team_span.get_text(strip=True)
-        print("Name:", name)
-        print("Team:", team)
-        print()
-    
-    data_cells = row.find_all('td')
-    for cell in data_cells:
-        print(cell.text, end='\t')
-    print()  # Newline after each row
     
 # table heading lines
 th_elements = TAB.find('thead').find_all('th')
@@ -79,11 +30,61 @@ column_titles = [th.find('button').get('title') for th in th_elements if th.find
 # Add "Name" and "Club" at the start of the list
 column_titles = ["Name", "Club"] + column_titles
 
-# Print the column titles
-print(column_titles)
+# Funcion to remove commas in the data so number >999 are interpeted as a single number using regular expressions
+def remove_commas(text):
+    return re.sub(r',','', text)
 
+# Function to remove leading/trailing brackets
+def strip_brackets(text):
+    return re.sub(r'^\(|\)$', '', text)
+
+# Create and open a csv file to write data into and assign it to the variable 'file'
 with open('Super_Six_Data.csv', 'w') as file:
+    
+    # Write the column titles into the csv file joined by commas
     file.write(','.join(column_titles))
-
+    
+    # Iterate through the rows to extract the data
+    for row in table_rows:
+        print(row)
+        
+        # Empty list to add the rows data to
+        row_contents = []
+        
+        # Retrieving the name and team elements as it is not in a data field
+        name_span = row.find('span', class_='fiso-lab-table__row-heading-primary-data')
+        team_span = row.find('span', class_='fiso-lab-table__row-heading-secondary-data')
+        
+        # If both name_span and team_span found extract the text from the span elements stripping leading/trailing whitespace
+        if name_span and team_span:
+            name = name_span.get_text(strip=True)
+            team = strip_brackets(team_span.get_text(strip=True))
+            # Append the name and club to the row_contents list to then be added to the csv file
+            row_contents.append(name)
+            row_contents.append(team)
+            
+        # For the first row which is the titles to ensure another column isn't added
+        elif row.find('span', class_='fiso-lab-table__column-heading-text'):
+            None
+            
+        # If name_span and team_span not found assign value of not found to 'name' and 'team'
+        else:
+            row_contents.append("Name not found")
+            row_contents.append("Team not found")
+        
+        # Append the name and club to the row_contents list to then be added to the csv file
+        #row_contents.append(name)
+        #row_contents.append(team)
+        
+        # Making a list of all the data elements from the row
+        data_cells = row.find_all('td')
+        
+        # Iterate through the data elements within the row to extract the data from the row, 
+        # removing commas and leading/trailing whitespace then appending to the row_contents
+        for cell in data_cells:
+            (row_contents.append(remove_commas(cell.text.strip())))
+            
+        # Write the contents of the current row to the CSV file, joined by commas
+        file.write(','.join(row_contents) + '\n')
 
     
